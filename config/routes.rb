14fa6b77766137355d6094833307e9a314b7f84a
SYwebcamp <122337092+SYwebcamp@users.noreply.github.com>
2023-02-21 13:21:17 +0000

    [Add]viewとcontrollerの基本的機能の追加

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,9 @@
 Rails.application.routes.draw do
+  resources :users, only: [:show, :edit, :update]
+  resources :books, only: [:new, :index, :show, :create, :destroy]
   devise_for :users
-  root to: 'homes#top'
+  #devise_for :users'homes#top'
+  root to: "homes#top"
+  get 'homes/about', as: 'about'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
